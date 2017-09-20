07dd93553376c64b75f933630889bc184c9fc5fa
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2017-09-20 14:18:50 +0200

    Moved consumer to own file and cleanup

---
+++ include/rtlog/Consumer.hpp
@@ -0,0 +1,65 @@
+/** \file
+ *
+ */
+
+#pragma once
+
+#include <atomic>
+#include <chrono>
+#include <fstream>
+#include <functional>
+#include <thread>
+
+#include "Formatter.hpp"
+#include "../Traits.hpp"
+
+namespace rtlog {
+
+template<typename LOGGER_TRAITS, typename QUEUE_TRAITS>
+class CLogConsumerT
+{
+protected:
+    moodycamel::ConcurrentQueue<rtlog::ArgumentArrayT<LOGGER_TRAITS>, QUEUE_TRAITS>& m_Queue;
+    std::chrono::microseconds m_PollInterval;
+    rtlog::CFormatter m_Formatter;
+    std::thread m_ConsumerThread;
+    std::atomic_bool m_Stop;
+    std::string m_FileName;
+    std::ofstream m_Stream;
+
+public:
+    typedef moodycamel::ConcurrentQueue<rtlog::ArgumentArrayT<LOGGER_TRAITS>, QUEUE_TRAITS> queue_type;
+
+    CLogConsumerT(const std::string& filename, queue_type& queue, uint32_t poll_interval_us) :
+        m_Queue(queue), m_PollInterval(poll_interval_us), m_FileName(filename),
+        m_Stream(filename, std::ofstream::binary|std::ofstream::trunc|std::ofstream::out)
+    {
+        m_Stop.store(false);
+        m_ConsumerThread = std::thread(std::bind(&CLogConsumerT<LOGGER_TRAITS, QUEUE_TRAITS>::consume, this));
+    }
+
+    void consume()
+    {
+        const char* p;
+        while (!m_Stop.load(std::memory_order_acquire)) {
+            while ((p = m_Formatter.format(m_Queue)) != NULL)
+                m_Stream << p;
+
+            std::this_thread::sleep_for(m_PollInterval);
+        }
+
+        while ((p = m_Formatter.format(m_Queue)) != NULL)
+            m_Stream << p;
+        m_Stream.flush();
+    }
+
+    void stop()
+    {
+        m_Stop.store(true);
+        m_ConsumerThread.join();
+        m_Stream.close();
+    }
+};
+using CLogConsumer = CLogConsumerT<rtlog::logger_traits, rtlog::ConcurrentQueueTraits>;
+
+}  // namespace rtlog
--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,6 @@
 #include "../include/stdafx.h"
 #include "../include/rtlog/Formatter.hpp"
+#include "../include/rtlog/Consumer.hpp"
 #include "../include/rtlog/rtlog.hpp"
 
 #ifndef DBG
@@ -18,66 +19,24 @@ bool is_logged(LogLevel level)
 }  // namespace rtlog
 
 
-template<typename QUEUE_TRAITS>
-class CLogConsumerT
-{
-protected:
-    moodycamel::ConcurrentQueue<rtlog::Argument, QUEUE_TRAITS>& m_Queue;
-    std::chrono::microseconds m_PollInterval;
-    rtlog::CFormatter m_Formatter;
-    std::thread m_ConsumerThread;
-    std::atomic_bool m_Stop;
-
-public:
-    CLogConsumerT(moodycamel::ConcurrentQueue<rtlog::Argument, QUEUE_TRAITS>& queue, uint32_t poll_interval) :
-        m_Queue(queue), m_PollInterval(poll_interval)
-    {
-        m_Stop.store(false);
-        m_ConsumerThread = std::thread(std::bind(&CLogConsumerT<QUEUE_TRAITS>::consume, this));
-    }
-
-    void consume()
-    {
-        const char* p;
-        while (!m_Stop.load(std::memory_order_acquire)) {
-            p = m_Formatter.format(m_Queue);
-            if (p)
-                std::cout << p;
-            else
-                std::this_thread::sleep_for(m_PollInterval);
-        }
-
-        while ((p = m_Formatter.format(m_Queue)) != NULL)
-            std::cout << p;
-    }
-
-    void stop()
-    {
-        m_Stop.store(true);
-        m_ConsumerThread.join();
-    }
-};
-using CLogConsumer = CLogConsumerT<rtlog::ConcurrentQueueTraits>;
-
 int main(int argc, char* argv[])
 {
-    moodycamel::ConcurrentQueue<rtlog::Argument, rtlog::ConcurrentQueueTraits> main_queue;
+    moodycamel::ConcurrentQueue<rtlog::ArgumentArray, rtlog::ConcurrentQueueTraits> main_queue;
     rtlog::CFormatter formatter;
 
     rtlog::CLogger::initialize(main_queue);
-    CLogConsumer consumer(main_queue, 600);
-    //~ std::thread consumer_thread(consumer, std::ref(formatter), std::ref(main_queue));
+    rtlog::CLogConsumer consumer("test.log", main_queue, 600);
 
     std::random_device r;
     std::default_random_engine e1(r());
     std::uniform_int_distribution<unsigned int> uniform_dist(10, 200);
 
+
     for (int i{0}; i < 100; i++) {
         for (int j{0}; j < 10; j++) {
-            LOG_INFO("User message", (i*10)+j);
-            std::this_thread::sleep_for(
-                std::chrono::microseconds(uniform_dist(e1))
-            );
+            std::chrono::microseconds sleep_time(uniform_dist(e1));
+            LOG_INFO("User message", (i*10)+j, sleep_time.count());
+            std::this_thread::sleep_for(sleep_time);
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
