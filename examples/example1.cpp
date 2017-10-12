ac63682e988e36103e84217e308f13021b880dd4
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2017-10-12 17:02:30 +0200

    Adapted to CLogger changes

--- examples/example1.cpp
+++ examples/example1.cpp
@@ -1,40 +1,14 @@
 // follow with
 // strace -o log -ff -tt
 #include "../include/stdafx.h"
-#include "../include/rtlog/Formatter.hpp"
 #include "../include/rtlog/Consumer.hpp"
 #include "../include/rtlog/rtlog.hpp"
-
-#ifndef DBG
-#define DBG() printf("%s:%d - %s\n", __FILE__, __LINE__, static_cast<const char*>(__PRETTY_FUNCTION__))
-#endif
-
-namespace rtlog
-{
-
-std::atomic<unsigned int> loglevel = {0};
-bool is_logged(LogLevel level)
-{
-    return static_cast<unsigned int>(level) >= loglevel.load(std::memory_order_relaxed);
-}
-
-template
-fmt::BasicWriter<char>& operator<<(fmt::BasicWriter<char>& os, Argument const& arg);
-
-#if defined(BOOST_LIB_C_GNU) && defined(BOOST_OS_LINUX) && defined(USE_INTERNAL_GETTID)
-size_t __thread_id_offset_linux_glibc = rtlog::__find_thread_id_offset();
-#endif
-
-}  // namespace rtlog
 
 
 int main(int argc, char* argv[])
 {
-    moodycamel::ConcurrentQueue<rtlog::ArgumentArray, rtlog::ConcurrentQueueTraits> main_queue;
-    rtlog::CFormatter formatter;
-
-    rtlog::CLogger::initialize(main_queue);
-    rtlog::CLogConsumer consumer("test.log", main_queue, 600);
+    auto& logger = rtlog::CLogger::initialize(rtlog::LogLevel::INFO);
+    rtlog::CLogConsumerSingleFile consumer("test.log", logger.getQueue(), 600);
 
     std::random_device r;
     std::default_random_engine e1(r());
