de4d01d7c6da0185dd7af3c548213d09140867b8
Giuseppe Corbelli <corbelligiuseppe@mesdan.it>
2017-10-12 16:58:34 +0200

    Added DEFAULT_LEVEL to LoggerTraits struct

--- include/Traits.hpp
+++ include/Traits.hpp
@@ -5,20 +5,28 @@
 #pragma once
 
 #include "concurrentqueue.h"
+#include "rtlog/Levels.hpp"
 
 namespace rtlog {
 
+/** Configuration parameters for the concurrent queue */
 struct ConcurrentQueueTraits : public moodycamel::ConcurrentQueueDefaultTraits
 {
     static const std::size_t BLOCK_SIZE = 32;
     static const std::size_t MAX_SUBQUEUE_SIZE = 64;
 };
 
-struct logger_traits
+/** Configuration parameters for the logger itself */
+struct LoggerTraits
 {
+    /** Number of available parameters in RTLOG_ macros */
     constexpr static std::size_t PARAM_SIZE = 16;
+    /** Formatting buffer size */
     constexpr static std::size_t BUFFER_SIZE = 1024;
+    /** Default logger level */
+    constexpr static LogLevel DEFAULT_LEVEL = LogLevel::INFO;
 
+    /** Messages character type */
     typedef char CHAR_TYPE;
 };
 
